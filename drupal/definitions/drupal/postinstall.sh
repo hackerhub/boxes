7da9fdb29b77196f550184869596fb323b113f17
Jair Gaxiola <jyr.gaxiola@gmail.com>
2012-04-15 21:58:41 -0500

    Drupal config

--- drupal/definitions/drupal/postinstall.sh
+++ drupal/definitions/drupal/postinstall.sh
@@ -31,13 +31,13 @@ apt-get -y install drush
 
 # Drupal config on nginx
 cd /etc/nginx/conf.d/
-wget -c https://raw.github.com/gist/2388530/744b58d090c856e40ca931c00737ac40f5bffca5/php.conf
-wget -c https://raw.github.com/gist/2388123/53a866c16388ba8589f51d462884743f0e2f4574/drupal.conf
+sudo wget -c https://raw.github.com/gist/2388530/744b58d090c856e40ca931c00737ac40f5bffca5/php
+sudo wget -c https://raw.github.com/gist/2388123/53a866c16388ba8589f51d462884743f0e2f4574/drupal
 
 # Vhost training.dev
-cd /etc/nginx/sites-availables/
-wget -c https://raw.github.com/gist/2388457/a27ad8fbe1eb8e8c52082394a31dd03093b596e5/training.dev
-ln -s /etc/nginx/sites-available/training.dev /etc/nginx/sites-enabled/training.dev
+cd /etc/nginx/sites-available/
+sudo wget -c https://raw.github.com/gist/2388457/a27ad8fbe1eb8e8c52082394a31dd03093b596e5/training.dev
+sudo ln -s /etc/nginx/sites-available/training.dev /etc/nginx/sites-enabled/training.dev
 echo "127.0.0.1     training.dev" >> /etc/hosts
 
 # Reload services
